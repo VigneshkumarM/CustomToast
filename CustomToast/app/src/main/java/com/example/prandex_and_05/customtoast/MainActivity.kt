5512f90f4c0117991133958d933ca5ed9d7b219f
VigneshkumarM <vignesh@jovee.in>
2018-10-28 11:52:10 +0530

    add alert toast with options

--- CustomToast/app/src/main/java/com/example/prandex_and_05/customtoast/MainActivity.kt
+++ CustomToast/app/src/main/java/com/example/prandex_and_05/customtoast/MainActivity.kt
@@ -2,11 +2,19 @@ package com.example.prandex_and_05.customtoast
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.customtoast.DToast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button1.setOnClickListener {
+            DToast.normal(this,"Hello",Toast.LENGTH_LONG,"#000000","#ffffff")
+        }
+
     }
 }
--- CustomToast/customtoast/src/main/java/com/example/customtoast/DToast.kt
+++ CustomToast/customtoast/src/main/java/com/example/customtoast/DToast.kt
@@ -8,24 +8,77 @@ import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
+//import javax.swing.text.StyleConstants.getBackground
+import android.widget.LinearLayout
+//import javax.swing.text.StyleConstants.getBackground
+import android.graphics.drawable.ColorDrawable
+import android.graphics.drawable.GradientDrawable
+import android.graphics.drawable.ShapeDrawable
 
 class DToast {
 
-    fun normalToast(mContext : Context, message : String)
-    {
-        val context = mContext
-        val inflater = (mContext as Activity).layoutInflater
-        val customToastroot = inflater.inflate(R.layout.toast_layout, null)
+    companion object {
 
-        //customToastroot.setBackgroundColor(Color.parseColor(color))
-        val text = customToastroot.findViewById<TextView>(R.id.custom_toast_textView) as TextView
-        text.text = "  $message  "
+        fun normal(mContext: Context, message: String, duration: Int, backColor: String, textColor: String) {
+            val inflater = (mContext as Activity).layoutInflater
+            val customToastroot = inflater.inflate(R.layout.toast_layout, null)
 
-        val customtoast = Toast(mContext)
-        customtoast.view = customToastroot
-        customtoast.setGravity(Gravity.BOTTOM, 0, 80)
-        customtoast.duration = Toast.LENGTH_SHORT
-        customtoast.show()
+            val text = customToastroot.findViewById<TextView>(R.id.custom_toast_textView) as TextView
+            val layout = customToastroot.findViewById<TextView>(R.id.main_layout) as LinearLayout
+            val image = customToastroot.findViewById<ImageView>(R.id.custom_toast_imageView) as ImageView
+            image.visibility = View.GONE
+            val background = layout.background
+            text.text = "  $message  "
+            text.setTextColor(Color.parseColor(textColor))
+
+            if (background is ShapeDrawable) {
+                // cast to 'ShapeDrawable'
+                background.paint.color = Color.parseColor(backColor)
+            } else if (background is GradientDrawable) {
+                // cast to 'GradientDrawable'
+                background.setColor(Color.parseColor(backColor))
+            } else if (background is ColorDrawable) {
+                // alpha value may need to be set again after this call
+                background.color = Color.parseColor(backColor)
+            }
+
+            val customtoast = Toast(mContext)
+            customtoast.view = customToastroot
+            customtoast.setGravity(Gravity.BOTTOM, 0, 80)
+            customtoast.duration = duration
+            customtoast.show()
+        }
+
+        fun alert(mContext: Context, message: String, duration: Int,backColor: String, textColor: String) {
+            val inflater = (mContext as Activity).layoutInflater
+            val customToastroot = inflater.inflate(R.layout.toast_layout, null)
+
+            val text = customToastroot.findViewById<TextView>(R.id.custom_toast_textView) as TextView
+            val layout = customToastroot.findViewById<TextView>(R.id.main_layout) as LinearLayout
+            val image = customToastroot.findViewById<ImageView>(R.id.custom_toast_imageView) as ImageView
+            image.setImageResource(R.mipmap.alert_icon)
+            val background = layout.background
+            text.text = " $message  "
+            text.setTextColor(Color.parseColor(textColor))
+
+            if (background is ShapeDrawable) {
+                // cast to 'ShapeDrawable'
+                background.paint.color = Color.parseColor(backColor)
+            } else if (background is GradientDrawable) {
+                // cast to 'GradientDrawable'
+                background.setColor(Color.parseColor(backColor))
+            } else if (background is ColorDrawable) {
+                // alpha value may need to be set again after this call
+                background.color = Color.parseColor(backColor)
+            }
+
+            val customtoast = Toast(mContext)
+            customtoast.view = customToastroot
+            customtoast.setGravity(Gravity.BOTTOM, 0, 80)
+            customtoast.duration = duration
+            customtoast.show()
+        }
+
     }
 
 }
